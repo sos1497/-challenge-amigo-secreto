8d21706482476f3cb7ecf2749dcf6eeca90d9f9e
sos1497 <moralesmateo277@gmail.com>
2025-09-21 13:27:48 -0500

    Definicion de funciones en app.js y la lista amigo, ademas de crear la funcion agregarAmigo

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = []
+
+function agregarAmigo () {
+    let nombreAmigo= document.getElementById("amigo").value;
+    if (nombreAmigo==''){
+        alert("Por favor, inserte un nombre.")
+    } 
+    return amigos.push(nombreAmigo)
+}
+
+function listaAmigo () {
+    
+
+}
+
+function sortearAmigo () {
+
+
+}
