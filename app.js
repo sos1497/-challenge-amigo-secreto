ba920ea72799dd1942e2883de773a27b24b83be1
sos1497 <moralesmateo277@gmail.com>
2025-09-21 14:10:14 -0500

    Creacion de la funcion listaAmigo y actualizacion de la funcion agregarAmigo

--- app.js
+++ app.js
@@ -5,16 +5,25 @@ function agregarAmigo () {
     let nombreAmigo= document.getElementById("amigo").value;
     if (nombreAmigo==''){
         alert("Por favor, inserte un nombre.")
+        return;
     } 
-    return amigos.push(nombreAmigo)
+    amigos.push(nombreAmigo)
+    document.querySelector('#amigo').value='';
+    listaAmigo();
 }
 
 function listaAmigo () {
     
-
+    let listaAmigo=document.getElementById("listaAmigos");
+        listaAmigo.innerHTML=""
+    for (let i=0; i<amigos.length; i++) {
+        let amigo= document.createElement("li");
+        amigo.textContent = amigos[i];
+        listaAmigo.appendChild(amigo);
+    }
+    
 }
 
 function sortearAmigo () {
-
 
 }
